b27724cc3d14dd26d3aac34396df051e8dea45dd
BuildTools <unconfigured@null.spigotmc.org>
2022-01-25 02:23:58 -0600

    + Addable Body Parts (only up to like 6).

--- snake.js
+++ snake.js
@@ -21,17 +21,57 @@ const DIRECTIONS = {
         val: 3
     }
 }
+
+const ITEM = {
+    APPLE: {
+        color: 0xff0000,
+        str: "Apple",
+        action: function() {
+            currentGame.addPart();
+        }
+    }
+}
+
 const GAME_WIDTH = 600;
 const GAME_HEIGHT = 600;
 const TILE_SIZE = 25;
 const COLS = GAME_WIDTH / TILE_SIZE;
 const ROWS = GAME_HEIGHT / TILE_SIZE;
-const GAME_SPEED = 5;
-const APPLE_COUNT = 2;
+const APPLE_COUNT = 1;
 
 const Tile_Grid = [];
 const Walls = [];
-const Apples = [];
+const Items = [];
+
+class Item {
+    constructor(x, y, z, type) {
+        this.type = type;
+
+        this.geometry = new THREE.SphereGeometry(5, 5, 5);
+        this.material = new THREE.MeshPhongMaterial({color: this.type.color});
+        this.mesh = new THREE.Mesh(this.geometry, this.material);
+
+        this.mesh.position.x = x;
+        this.mesh.position.y = y;
+        this.mesh.position.z = z;
+
+        this.expended = false;
+        currentScene.add(this.mesh);
+    }
+    update() {
+        if(!this.expended) {
+            this.type.action();
+            this.expended = true;
+            // Remove item after its action is performed;
+            let i = Items.indexOf(this);
+            if(i !== -1) {
+                Items.splice(i, 1);
+                currentScene.remove(this.mesh);
+            }
+        }
+    }
+}
+
 class Tile {
     constructor(x = 0, y = 0, z = 0, color = 0xffffff) {
         this.geometry = new THREE.PlaneGeometry(TILE_SIZE, TILE_SIZE);
@@ -43,10 +83,24 @@ class Tile {
         this.mesh.position.z = z;
 
         this.headPresent = false;
+        this.item = null;
+    }
+
+    assignItem(type) {
+        let pos = this.mesh.position;
+        this.item = new Item(pos.x, pos.y, pos.z, type);
+        Items.push(this.item);
     }
 
     update() {
-
+        this.checkOccupants();
+        if(this.item != null) {
+            if(this.headPresent) {
+                // Do item action
+                this.item.update();
+                this.item = null;
+            }
+        }
     }
 
     checkOccupants() {
@@ -65,18 +119,75 @@ class Tile {
         }
     }
 }
-class Apple {
-    constructor() {
-    }
-}
 
 class SnakePart {
+    constructor(pos, initialDirection) {
+        this.geometry = new THREE.SphereGeometry(13, 32, 16);
+        this.material = new THREE.MeshPhongMaterial({color: 0x00ff00});
+        this.mesh = new THREE.Mesh(this.geometry, this.material);
 
+        this.mesh.position.x = pos.x;
+        this.mesh.position.y = pos.y;
+        this.mesh.position.z = pos.z;
+
+        this.direction = initialDirection;
+        this.velocity = {
+            x: 2.5,
+            y: 2.5
+        }
+
+        this.placed = false;
+
+        this.actions = [];
+
+        currentScene.add(this.mesh);
+    }
+    moveWhen(pos, direction) {
+        this.actions.push({pos: pos, direction: direction});
+    }
+    update() {
+        for (let action of this.actions) {
+            if(action.pos.x === this.mesh.position.x && action.pos.y === this.mesh.position.y) {
+                let index = this.actions.indexOf(action);
+                this.actions.splice(index, 1);
+                this.direction = action.direction;
+            }
+        }
+        this.move(this.direction);
+    }
+    move(direction) {
+        switch (direction.val) {
+            case 2:
+                this.up();
+                break;
+            case 4:
+                this.down();
+                break;
+            case 1:
+                this.left();
+                break;
+            case 3:
+                this.right();
+                break;
+        }
+    }
+    up() {
+        this.mesh.position.y = this.mesh.position.y + this.velocity.y;
+    }
+    down() {
+        this.mesh.position.y = this.mesh.position.y - this.velocity.y;
+    }
+    left() {
+        this.mesh.position.x = this.mesh.position.x - this.velocity.x;
+    }
+    right() {
+        this.mesh.position.x = this.mesh.position.x + this.velocity.x;
+    }
 }
 
 class SnakeHead {
     constructor(color=0x0000ff) {
-        this.geometry = new THREE.SphereGeometry(13, 32, 16);
+        this.geometry = new THREE.SphereGeometry(15, 32, 16);
         this.material = new THREE.MeshPhongMaterial({color: color});
         this.mesh = new THREE.Mesh(this.geometry, this.material);
 
@@ -89,8 +200,8 @@ class SnakeHead {
         this.directionChange = DIRECTIONS.RIGHT;
         this.direction = DIRECTIONS.RIGHT;
         this.velocity = {
-            x: 1.0,
-            y: 1.0
+            x: 2.5,
+            y: 2.5
         }
         snakeScene.add(this.mesh);
     }
@@ -108,7 +219,15 @@ class SnakeHead {
             } else if(centeredOnTile(this.mesh.position)) {
                 // Change Direction
                 this.direction = this.directionChange;
+
+                // Instruct all parts to make same move.
+                let pos = this.mesh.position;
+                let dir = this.direction;
+                currentGame.allPartsMoveWhen(JSON.parse(JSON.stringify(pos)), JSON.parse(JSON.stringify(dir)));
+
                 this.move(this.direction);
+
+
             } else {
                 this.move(this.direction);
             }
@@ -162,21 +281,68 @@ class SnakeGame {
     constructor() {
         this.head = new SnakeHead();
         this.inProgress = false;
+
+        this.queuedParts = 0;
+        this.parts = [];
+    }
+    addPart() {
+        this.queuedParts++;
+    }
+    allPartsMoveWhen(pos, direction) {
+        for(let i = 0; i <= this.parts.length-1; i++) {
+            let p = this.parts[i];
+            p.moveWhen(pos, direction);
+        }
     }
     update() {
         if(this.inProgress) {
+            for(let i = 0; i <= this.parts.length-1; i++) {
+                let p = this.parts[i];
+                p.update();
+            }
+            if(this.queuedParts > 0) {
+                // There are parts to add.
+                if(centeredOnTile(this.head.mesh.position)) {
+                    // Parts can only be added when the head is centered on a tile.
+                    if(this.parts.length <= 0) {
+                        // There are no parts, use head to determine spawn spot.
+                        let opposingDirection = getOpposingDirection(currentGame.head.direction.val);
+                        let findHeadTileDetails = findTile(function(tile) {
+                            return tile.headPresent;
+                        });
+                        let newPartTile = Tile_Grid[getTileShiftedIndex(findHeadTileDetails.index, opposingDirection)];
+                        this.parts.push(new SnakePart(newPartTile.mesh.position, this.head.direction));
+                        this.queuedParts--;
+                    } else {
+                        // There are parts, use last part added to determine next spot.
+                        let lastPart = this.parts[this.parts.length-1];
+                        if(centeredOnTile(lastPart.mesh.position)) {
+                            let findTileDetails = findTile(function(tile) {
+                                if(tile.mesh.position.x === lastPart.mesh.position.x && tile.mesh.position.y === lastPart.mesh.position.y) {
+                                    return true;
+                                }
+                            });
+                            let opposingDirection = getOpposingDirection(lastPart.direction.val);
+                            let newPartTile = Tile_Grid[getTileShiftedIndex(findTileDetails.index, opposingDirection)];
+                            this.parts.push(new SnakePart(newPartTile.mesh.position, lastPart.direction));
+                            this.queuedParts--;
+                        }
+                    }
+                }
+            }
+
             this.head.update();
         }
     }
     start() {
         this.inProgress = true;
-        this.clock = setInterval(() => {
-            this.update();
-        }, GAME_SPEED);
+        // this.clock = setInterval(() => {
+        //     this.update();
+        // }, GAME_SPEED);
     }
     stop() {
         this.inProgress = false;
-        clearInterval(this.clock);
+        // clearInterval(this.clock);
     }
 }
 
@@ -193,6 +359,16 @@ function isEven(n) {
 
 function isOdd(n) {
     return Math.abs(n % 2) === 1;
+}
+
+function findTile(validateFunc) {
+    for (let i = 0; i < Tile_Grid.length-1; i++) {
+        let t = Tile_Grid[i];
+        let valid = validateFunc(t);
+        if(valid) {
+            return { index: i, tile: t};
+        }
+    }
 }
 
 let currentGame;
@@ -221,10 +397,39 @@ function animate() {
     requestAnimationFrame(animate);
     Renderer.render(currentScene, Camera);
 
-    for (const tileGridElement of Tile_Grid) {
-        tileGridElement.update();
-        tileGridElement.checkOccupants();
+    currentGame.update();
+
+    handleAppleItems();
+
+    for (const tile of Tile_Grid) {
+        tile.update();
     }
+}
+
+function handleAppleItems() {
+    let presentApples = 0;
+    for (let tile of Tile_Grid) {
+        if(tile.item != null && tile.item.type === ITEM.APPLE) {
+            presentApples++;
+        }
+    }
+    for(let i = 0; i < APPLE_COUNT - presentApples; i++) {
+        let tile = getTileNoItem();
+        tile.assignItem(ITEM.APPLE);
+    }
+}
+
+function getTileNoItem() {
+    while(true) {
+        let i = Math.floor(randInRange(0, Tile_Grid.length-1));
+        if(Tile_Grid[i].item === null) {
+            return Tile_Grid[i];
+        }
+    }
+}
+
+function randInRange(min, max) {
+    return Math.random() * (max - min) + min;
 }
 
 function wallsAndTiles() {
@@ -241,7 +446,7 @@ function wallsAndTiles() {
     }
     // * Wall Lines
     let lineWidth = 5;
-    const wallMaterial = new THREE.LineBasicMaterial({color: 0xff0000, linewidth: lineWidth});
+    const wallMaterial = new THREE.LineBasicMaterial({color: 0xff8000, linewidth: lineWidth});
     const points = [];
     points.push(new THREE.Vector3(-xOffset - (TILE_SIZE/2), yOffset + (TILE_SIZE/2), 0));
     points.push(new THREE.Vector3(xOffset + (TILE_SIZE/2), yOffset + (TILE_SIZE/2), 0));
@@ -252,6 +457,34 @@ function wallsAndTiles() {
     const line = new THREE.Line(geometry, wallMaterial);
     Walls.push(line);
     snakeScene.add(line);
+}
+
+function getTileShiftedIndex(startIndex, direction) {
+    switch(direction) {
+        case 1:
+            return startIndex-1;
+        case 2:
+            return startIndex+ROWS;
+        case 3:
+            return startIndex+1;
+
+        case 4:
+            return startIndex-ROWS;
+
+    }
+}
+
+function getOpposingDirection(dir) {
+    switch (dir) {
+        case 1:
+            return 3;
+        case 2:
+            return 4;
+        case 3:
+            return 1;
+        case 4:
+            return 2;
+    }
 }
 
 addEventListener('resize', () => {
